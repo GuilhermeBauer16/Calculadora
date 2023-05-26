67ac38e2ccb1fffc2e824e2858aeedcfb1421053
Guilherme <guilhermebauer16@gmail.com>
2023-05-26 17:20:50 -0300

    Terminando a inserção dos botões

--- main.py
+++ main.py
@@ -1,185 +1,115 @@
 from tkinter import *
+#funções 
+def click_btn(numero):
+     entrada.delete(0 ,END)
+     entrada.insert(END ,numero)
+
+def dividir():
+     global primeiro_numero
+     primeiro_numero = int(entrada.get())
+     entrada.delete( 0 , END)
+
+
+def calcular():
+     segundo_numero = int(entrada.get())
+     resultado = primeiro_numero / segundo_numero
+     entrada.delete(0 , END)
+     entrada.insert( 0 , resultado)
+
+
+def apaga():
+     entrada.delete(0 , END)
+
 
 janela = Tk()
 janela.title("Calculadora")
 janela.config(padx= 2, pady= 2 , background='#AAA')
 janela.resizable(False, False)
-etiqueta = Label(janela , text="Ainda não possue nenhuma conta", justify=CENTER,
-font=('fontana', 16 , 'bold'), bg='#AAA') 
+primeiro_numero = 0
 
-entrada = Entry(janela, width=15 , borderwidth=6, relief=SOLID, bg='#FFFFFF', fg='#000',
-font=('futura',25, "bold"), justify=CENTER )
-# etiqueta.grid(
-#     row=0,
-#     column=4)
+entrada = Entry(janela, 
+               width=15 , 
+               borderwidth=6, 
+               relief=SOLID, 
+               bg='#FFFFFF', 
+               fg='#000',
+               font=('futura',30, "bold"), 
+               justify=CENTER,) 
+
 entrada.grid(
     row=0,
     column=0,
     columnspan=3,
-    pady= 2
+    pady= 6,
+    padx= 10
 )
-#funções 
-def click_btn():
-    return 
+
+def cria_btn(aparece,texto,espacox,espacoy,fundo,fonte,click,commando,style):
+
+     botao = Button(aparece,
+                    text=texto,
+                    font=('fontana', 12 , 'bold'),
+                    padx=espacox ,
+                    pady=espacoy ,
+                    bg= fundo, 
+                    fg= fonte,
+                    activebackground= click,
+                    command= commando,
+                    relief= style)
+     
+     return botao
+
+def cria_grid(variavel,coluna,linha , espacox=0,espacoy=5, espaco=1):
+     variavel.grid(row= coluna,column= linha , padx= espacox , pady= espacoy ,columnspan= espaco)
 
 
-# botões 
-divide = Button(janela,
-                text="÷" , font=('fontana' , 12 , 'bold'),padx=20 , pady= 20,
-                bg='#FFDB58', fg="#000000" , relief= FLAT, activebackground='#FFA500')
+um = cria_btn(janela,"1",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(1), FLAT)
+cria_grid(um,1 ,0,0,5)
 
-divide.grid(row= 0 , column=3 ,padx=0 , pady= 5  )
+dois = cria_btn(janela,"2",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(2), FLAT)
+cria_grid(dois,1 ,1,0,5)
 
-um = Button(janela , 
-                text='1', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(1))
+tres = cria_btn(janela,"3",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(3), FLAT)
+cria_grid(tres,1 ,2,0,5)
 
-um.grid(
-     row=1 , 
-     column=0 , 
-     padx= 0  , 
-     pady= 5)
+quatro = cria_btn(janela,"4",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(4), FLAT)
+cria_grid(quatro,2 ,0,0,5)
 
+cinco = cria_btn(janela,"5",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(5), FLAT)
+cria_grid(cinco,2 ,1,0,5)
 
-dois = Button(janela , 
-                text='2', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(2))
+seis = cria_btn(janela,"6",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(6), FLAT)
+cria_grid(seis,2 ,2,0,5)
 
-dois.grid(
-     row=1 , 
-     column=1 , 
-     padx= 0  , 
-     pady= 5)
+sete = cria_btn(janela,"7",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(7), FLAT)
+cria_grid(sete,3 ,0,0,5)
 
-tres = Button(janela , 
-                text='3', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(3))
+oito = cria_btn(janela,"8",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(8), FLAT)
+cria_grid(oito,3 ,1,0,5)
 
-tres.grid(
-     row=1 , 
-     column=2 , 
-     padx= 0  , 
-     pady= 5)
+nove = cria_btn(janela,"9",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(9), FLAT)
+cria_grid(nove,3 ,2,0,5)
 
-quatro = Button(janela , 
-                text='4', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(4))
+zero = cria_btn(janela,"0",90 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn(0), FLAT)
+cria_grid(zero,4 ,0,0,5, 2)
 
-quatro.grid(
-     row=2 , 
-     column=0 , 
-     padx= 0  , 
-     pady= 5)
+apagar = cria_btn(janela,"C",30 , 20 ,'#FFDB58',"#000000",'#FFA500',lambda:apaga(), FLAT)
+cria_grid(apagar,0 ,4,0,5,2)
 
-cinco = Button(janela , 
-                text='5', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(5))
+mais = cria_btn(janela,"+",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn('+'), FLAT)
+cria_grid(mais,1 ,3,0,5,2)
 
-cinco.grid(
-     row=2 , 
-     column=1 , 
-     padx= 0  , 
-     pady= 5)
+menos = cria_btn(janela,"-",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn('-'), FLAT)
+cria_grid(menos,2 ,3,0,5,2)
 
-seis = Button(janela , 
-                text='6', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(6))
+divide = cria_btn(janela,"÷",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:dividir(), FLAT)
+cria_grid(divide,3 ,3,0,5,2)
 
-seis.grid(
-     row=2 , 
-     column=2 , 
-     padx= 0  , 
-     pady= 5)
+igual = cria_btn(janela,"=",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:calcular(), FLAT)
+cria_grid(igual,4 ,3,0,5,2)
 
-sete = Button(janela , 
-                text='7', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(7))
+vezes = cria_btn(janela,"X",30 , 30 ,'#FFDB58',"#000000",'#FFA500',lambda:click_btn("X"), FLAT)
+cria_grid(vezes,4 ,2,0,5,2)
 
-sete.grid(
-     row=3 , 
-     column=0 , 
-     padx= 0  , 
-     pady= 5)
-
-oito = Button(janela , 
-                text='8', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(8))
-
-oito.grid(
-     row=3 , 
-     column=1 , 
-     padx= 0  , 
-     pady= 5)
-
-nove = Button(janela , 
-                text='9', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 20 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(9))
-
-nove.grid(
-     row=3 , 
-     column=2 , 
-     padx= 0  , 
-     pady= 5)
-
-zero = Button(janela , 
-                text='0', 
-                font=( 'fontana' , 12 , 'bold') , 
-                padx= 68 , pady= 20 ,
-                bg='#FFDB58', fg="#000000" , 
-                relief= FLAT, 
-                activebackground='#FFA500', 
-                command=lambda: click_btn(0))
-
-zero.grid(
-     row=4 , 
-     column=0, 
-     padx= 0  , 
-     pady= 5,
-     columnspan= 2)
 
 janela.mainloop()
